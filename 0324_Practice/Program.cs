8da1aedb7fb1b8130ce9ae8692d6de736e3e5fdd
ikarejaeyoung <wodud04277@gmail.com>
2025-03-24 20:01:55 +0900

    Set_start
    시작 테스트입니다.

---
+++ 0324_Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _0324_Practice
+{
+    internal class Program
+    {
+        class Trainer
+        {
+
+        }
+
+        class Monster
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
